fffd6f9ea42d8a4a21ddc1de813ed895e45d2ad1
Rus_sell9 <123436075+rus-sell9@users.noreply.github.com>
2026-09-16 17:27:25 -0700

    Add routing for Survey page in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import Survey from "./pages/Survey";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/survey" element={<Survey />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
